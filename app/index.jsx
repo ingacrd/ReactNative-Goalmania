0c495901e552a2aa711d665479f73dd4492b7781
Carolina Ruiz <Carolina.Ruiz@FutureElectronics.com>
2025-09-13 11:21:21 -0400

    remove babel, add GestureHandlerRootView

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,18 @@
 
 
 import { Link } from "expo-router"
-import { View, Text } from "react-native"
+import { View, Text, Pressable } from "react-native"
 
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-black">
             <Text className="text-3xl font-pregular text-white">BIENVENIDOS A GOLMANIA</Text>
-            <Link href="/home" style={{color: 'blue'}}>Go to Home</Link>
+            {/* <Link href="/home" style={{color: 'blue'}}>Go to Home</Link> */}
+
+            <Pressable onPress={() => router.push('/(tabs)')} className="px-4 py-3 bg-sky-500 rounded-lg" >
+            <Text className="text-white font-pmedium">Entrar</Text>
+            </Pressable>
         </View>
+       
     )
 }
