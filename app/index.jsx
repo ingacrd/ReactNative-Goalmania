bdb1cff04b9c736552ba4868e56e87f1f4d66f71
Carolina Ruiz <Carolina.Ruiz@FutureElectronics.com>
2025-09-21 09:59:50 -0400

    install NativeWind manually

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,18 @@
 
-
+import "./global.css"
 import { Link } from "expo-router"
 import { View, Text } from "react-native"
 
 export default function App() {
     return (
-        <View style={{flex:1, backgroundColor:'black', justifyContent:'center', alignItems:'center'}}>
-            <Text style={{fontSize:24, color:'white', fontFamily:'Poppins-SemiBold'}}>BIENVENIDOS A GOLMANIA</Text>
-            <Link href="/home" style={{color: 'blue'}}>Go to Home</Link>
+        // <View style={{flex:1, backgroundColor:'black', justifyContent:'center', alignItems:'center'}}>
+        //     <Text style={{fontSize:24, color:'white', fontFamily:'Poppins-SemiBold'}}>BIENVENIDOS A GOLMANIA</Text>
+        //     <Link href="/home" style={{color: 'blue'}}>Go to Home</Link>
+        // </View>
+        <View className="flex-1 items-center justify-center bg-red-500">
+            <Text className="text-xl font-bold text-blue-500">
+                Welcome to Nativewind!
+            </Text>
         </View>
     )
 }
